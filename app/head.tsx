030d31d0797537a88be81024a11924e64c66545c
limaxx <pkedditsoficial@gmail.com>
2025-11-24 14:06:40 -0300

    head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>StrikeMedia.Co</title>
+      <meta name="description" content="StrikeMedia.Co — viral music content" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
